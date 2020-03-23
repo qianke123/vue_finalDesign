dcc4337fecbb28d7f99ad26dfc7c7907b7c59a86
qianke <1150836154@qq.com>
2020-03-23 14:26:22 +0800

    初次提交

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+import './assets/css/global.css'
+import axios from 'axios'
+import qs from 'qs'
+
+Vue.prototype.$qs = qs
+
+Vue.prototype.$http = axios
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://localhost:8888'
+
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,50 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/login.vue'
+import Home from '../components/home.vue'
+import Welcome from '../components/welcome.vue'
+import Myfolder from '../components/myfolder.vue'
+import ShareCenter from '../components/shareCenter.vue'
+import Test from '../components/test.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
 const router = new VueRouter({
-  routes
+  routes: [
+    { path: '/test', component: Test },
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', component: Welcome },
+        { path: '/myfolder', component: Myfolder },
+        { path: '/shareCenter', component: ShareCenter }
+      ]
+    }
+  ]
+})
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数, 表示放行
+  // next() 放行 next('/login') 强制向login跳转
+  var token = window.sessionStorage.getItem('fileCloudToken')
+  if (to.path === '/login') {
+    if (!token) {
+      return next()
+    } else {
+      return next('/home')
+    }
+  } else if (!token) {
+    return next('/login')
+  } else {
+    return next()
+  }
 })
 
 export default router
